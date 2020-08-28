53aa64248f7619eac5628893a07cf674b75e50b0
Lyzw57 <askuczynski57@gmail.com>
2020-08-28 19:48:24 +0200

    created loop to find content

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ gearbox_type = "automatic"
 URL = f'https://www.olx.pl/motoryzacja/samochody/?search%5Bfilter_float_price%3Afrom%5D={min_price}&search%5Bfilter_float_price%3Ato%5D={max_price}&search%5Bfilter_enum_condition%5D%5B0%5D={is_damaged}&search%5Bfilter_enum_transmission%5D%5B0%5D={gearbox_type}'
 
 page = get(URL)
-bs = BeautifulSoup(page.content)
+bs = BeautifulSoup(page.content, 'html.parser')
 
 for offer in bs.find_all('div', class_='offer-wrapper'):
-    print(offer)
+    car_name = offer.find()
+    price = offer.find('p', class_='price').get_text()
+    footer = offer.find('td', class_='bottom-cell')
+    location = footer.find('small', class_="breadcrumb").get_text().strip().split(',')[0]
+    print(f"{location} - {price}")
